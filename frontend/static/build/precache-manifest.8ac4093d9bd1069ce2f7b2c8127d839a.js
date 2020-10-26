cc2a63ce1a849bb655d7d876f9cc30eb75b4559a
BrianCambron <69121396+BrianCambron@users.noreply.github.com>
2020-10-26 10:39:26 -0400

    deploying to heroku

---
+++ frontend/static/build/precache-manifest.8ac4093d9bd1069ce2f7b2c8127d839a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4120369afffa72bcf1d4def01a2fd70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "408d73af2c51e66b971f",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "e610a98ab4c6bd787288",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "408d73af2c51e66b971f",
+    "url": "/static/js/2.997165c1.chunk.js"
+  },
+  {
+    "revision": "99740f07e59da1777ca51e4cdac8fee9",
+    "url": "/static/js/2.997165c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e610a98ab4c6bd787288",
+    "url": "/static/js/main.a7856dbe.chunk.js"
+  },
+  {
+    "revision": "61e07dcfa1eb4aac95f5",
+    "url": "/static/js/runtime-main.34b54e52.js"
+  }
+]);
